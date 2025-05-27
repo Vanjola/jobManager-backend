3701e80f69efd53088a24f2f25546b06b312f24d
Vanjola <vanjagacic127@gmail.com>
2025-05-27 11:52:46 +0200

    inital Flask setup

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/",methods=["GET"])
+def home():
+    return "JobManager API is running!"
+
+
+
+
+
+
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
