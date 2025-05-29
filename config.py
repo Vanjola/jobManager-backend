e6786f2aa70c8a64c875a9048401b4d06f58ef71
Vanjola <150866146+Vanjola@users.noreply.github.com>
2025-05-29 13:31:00 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 BASE_DIR=os.path.abspath(os.path.dirname(__file__))
 load_dotenv()
 class Config:
-    SQLALCHEMY_DATABASE_URI='sqlite:///'+os.path.join(BASE_DIR,'jobmanager.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     JWT_SECRET_KEY=os.getenv("JWT_SECRET_KEY")
